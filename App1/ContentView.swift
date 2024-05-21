f6f1960d465011231afb3ca8b63b1847f79610a4
noigamegun <76461912+noigamegun@users.noreply.github.com>
2024-05-21 13:20:16 +0700

    Added Picture

--- App1/ContentView.swift
+++ App1/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            Image("Game")
+                .resizable()
+                .frame(width: 150,height: 150)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            Image("Person")
+                .resizable()
+                .frame(width: 150,height: 150)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+        }
         VStack {
             Text("Thapat Auechaikasem")
+                .frame(maxWidth: .infinity, maxHeight: 40)
                 .font(.system(size: 20, design: .rounded))
+                .fontWeight(.bold)
+                .background(.green)
+                .cornerRadius(5)
+                .foregroundStyle(.white)
+                .padding(.horizontal ,20)
             Text("Punnaphat Sittnanonporn")
+                .frame(maxWidth: .infinity, maxHeight: 40)
                 .font(.system(size: 20, design: .rounded))
-                .padding(.top, 5)
+                .fontWeight(.bold)
+                .background(.green)
+                .cornerRadius(5)
+                .foregroundStyle(.white)
+                .padding(.horizontal ,20)
         }
+        .padding(.top, 10)
+        
     }
 }
 
