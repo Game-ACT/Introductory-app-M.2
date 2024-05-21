0142616da49eaa5434d5624e6450a3e303fecc60
noigamegun <76461912+noigamegun@users.noreply.github.com>
2024-05-21 12:57:15 +0700

    Initial Commit

---
+++ App1/App1App.swift
@@ -0,0 +1,17 @@
+//
+//  App1App.swift
+//  App1
+//
+//  Created by Thapat Auechaikasem on 21/5/67.
+//
+
+import SwiftUI
+
+@main
+struct App1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App1
+//
+//  Created by Thapat Auechaikasem on 21/5/67.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
